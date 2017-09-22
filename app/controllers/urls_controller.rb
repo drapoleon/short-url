2a8d63f9e213283f7705b61bb31b7ac7391b4cc2
shashank <s.shashank36@gmail.com>
2017-09-22 15:45:21 +0530

    Comments to explain code
    Comments for data flow and logical operation in 'url.rb' and
    'urls_controller.rb'

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,31 +1,37 @@
 class UrlsController < ApplicationController
  
- def index
-  @url= Url.new
+ def index			
+  @url= Url.new   # creates new instance for URL class 
+  				  
+  #shows frequent URLs
   @url_new = Url.limit(100).group("main_url").order("count_main_url desc").count("main_url")
 
  end
 
  def show
+ 	#find main_url associated with given short_url
  	@url = Url.find_by_short_url(params[:short_url])	  
+ 	#redirects to the URL associated
 	redirect_to @url.main_url
  end
 
  def create
+   #take user data from index page 
    @url= Url.new(url_params)
-   @url.sanitize
+   @url.sanitize		#to remove white spaces and check URL
+   #generate short URL
    @url.short_url = @url.get_short_url(@url.get_last_id)
    if @url.save
-   	redirect_to urls_success_path
+   	redirect_to urls_success_path #page to show generated URL
    end
  end
 
  def success
-	@url = Url.last	
+	@url = Url.last	#fetches latest created URL data into 'Url instance'
  end
 
  private
-	def url_params
+	def url_params #whitelisting main_url
 		params.require(:url).permit(:main_url)
 	end
 
--- app/models/url.rb
+++ app/models/url.rb
@@ -1,34 +1,36 @@
 class Url < ApplicationRecord
   
- ALPHABET =
+ ALPHABET = #constant for URL generation
   	"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".split(//)
 
 
-  def get_short_url (last_id)
+  def get_short_url (last_id) #method to generate short_url
 
-	   return ALPHABET[0] if last_id == 0
-	   str = ''
-	   base = ALPHABET.length
-	   while last_id > 0
-	   	str << ALPHABET[last_id.modulo(base)]
-	   	last_id /= base
-	   end
-	   str.reverse
+  	#returns 'a' for first case(first URL generated)
+    return ALPHABET[0] if last_id == 0
+    str = ''
+    base = ALPHABET.length
+    while last_id > 0
+    str << ALPHABET[last_id.modulo(base)]
+    last_id /= base
+    end
+    str.reverse
   end
 
-  def get_last_id
+  def get_last_id #fetch id of last URL created for next URL generation
+    
     if Url.last
-    	id=Url.last.id
+      id=Url.last.id
     else
       id = 0
     end
   end
 
-  def sanitize
-	  self.main_url.strip!
-	  unless self.main_url[/\Ahttp:\/\//] || self.main_url[/\Ahttps:\/\//]
-       self.main_url = "http://#{self.main_url}"
-      end
+  def sanitize #clear white spaces from user data
+	self.main_url.strip!
+	unless self.main_url[/\Ahttp:\/\//] || self.main_url[/\Ahttps:\/\//]
+     self.main_url = "http://#{self.main_url}" #add 'http://' if absent
+    end
   end
 
 end
